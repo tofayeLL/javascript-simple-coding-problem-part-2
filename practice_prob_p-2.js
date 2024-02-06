a5c4a63275135b2a65ddd4dea9830e3f87b748e1
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-06 23:03:57 +0600

    finished practice task 5 from js practice problem part 2

--- practice_prob_p-2.js
+++ practice_prob_p-2.js
@@ -105,7 +105,35 @@ function findAveragePhonePrice(array) {
 
 }
 
-let avgPhonePrice = findAveragePhonePrice(phones);
-console.log(avgPhonePrice);
+// let avgPhonePrice = findAveragePhonePrice(phones);
+// console.log(avgPhonePrice);
+
+
+//-------------------------------------------------------//
+
+//-----problem 5----
+/* question: For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month. */
+
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+function getTotalSalary(employees) {
+    let total = 0;
+    for (let employee of employees) {
+
+        let eachEmployeeTotal = employee.starting + (employee.experience * employee.increment);
+
+        total = total + eachEmployeeTotal;
+    }
+    return total;
+
+}
+
+let totalMonthlySalary = getTotalSalary(employees);
+console.log('The total salary has to be provided by the company in a month:', totalMonthlySalary);
 
 
